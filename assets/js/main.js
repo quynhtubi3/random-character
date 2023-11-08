b104da8f281b776c4016f3d90a674195df0577cd
quynhtubi3 <idkr37@gmail.com>
2023-11-08 17:05:51 +0700

    abc

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,6 @@ function startRandom() {
 
   function chooseAgent(randomNum, i) {
     setTimeout(function () {
-      console.log(i);
       lstAgents[randomNum].classList.add("choosing");
       currentChoose.src = lstAgents[randomNum].src;
     }, i * 500);
@@ -66,5 +65,64 @@ function startRandom() {
     window.crypto.getRandomValues(array);
     var randomNum = array[0] % lstAgents.length;
     chooseAgent(randomNum, i);
+  }
+}
+
+function pickType(id) {
+  const box = document.querySelector(".random-box");
+  const boxes = document.querySelector(".random-boxes");
+  const choise1 = document.querySelector(".choise-1");
+  const choise2 = document.querySelector(".choise-2");
+  if (id === 1) {
+    box.classList.remove("active-box");
+    boxes.classList.remove("active-box");
+    box.classList.add("active-box");
+    choise1.classList.remove("choosing");
+    choise2.classList.remove("choosing");
+    choise1.classList.add("choosing");
+  }
+  if (id === 2) {
+    box.classList.remove("active-box");
+    boxes.classList.remove("active-box");
+    boxes.classList.add("active-box");
+    choise1.classList.remove("choosing");
+    choise2.classList.remove("choosing");
+    choise2.classList.add("choosing");
+  }
+}
+
+function randomATeam() {
+  const lstAgents = document.querySelectorAll(".all-agents .display img");
+  const lstRnd = document.querySelectorAll(".random-boxes .agents ul li img");
+  lstAgents.forEach((data) => {
+    data.classList.remove("choosed");
+  });
+  function chooseAgent(randomNum, i, j) {
+    setTimeout(function () {
+      lstAgents[randomNum].classList.add("choosing");
+      lstRnd[j].src = lstAgents[randomNum].src;
+    }, i * 500 * j);
+    setTimeout(function () {
+      lstAgents[randomNum].classList.remove("choosing");
+    }, i * 500 * j + 500);
+  }
+  function chooseEachAgent(i) {
+    var check = false;
+    while (!check) {
+      for (var j = 1; j <= 10; j++) {
+        var array = new Uint32Array(1);
+        window.crypto.getRandomValues(array);
+        var randomNum = array[0] % lstAgents.length;
+        chooseAgent(randomNum, j, i);
+      }
+      if (lstAgents[randomNum].classList.contains("choosed")) check = false;
+      else {
+        lstAgents[randomNum].classList.add("choosed");
+        check = true;
+      }
+    }
+  }
+  for (var i = 0; i <= 4; i++) {
+    chooseEachAgent(i);
   }
 }
